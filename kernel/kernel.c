c8e724ba3d58ed7c2095f8dcc780eeb07d2d0f2d
05st <05st@users.noreply.github.com>
2021-09-08 20:08:01 -0500

    Simple integer addition program

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,10 +1,13 @@
 #include "../drivers/video.h"
 #include "../cpu/isr.h"
 #include "../libc/string.h"
+#include "../libc/mem.h"
 
 int kmain() {
     isr_install();
     irq_install();
+
+    kprint("Ophanim\n\n");
 
     return 0;
 }
@@ -13,7 +16,34 @@ void handle_input(char* input) {
     if (strcmp(input, "END") == 0) {
         kprint("Halting...\n");
         asm volatile("hlt");
+    } else if (strcmp(input, "CLR") == 0) {
+        clrscr();
+    } else {
+        char* first_three[3];
+        memcpy(first_three, input, 3);
+        if (strcmp(first_three, "ADD") == 0) {
+            int a = 0;
+            int b = 0;
+            int index = 4;
+            while ((input[index] >= '0') && (input[index] <= '9')) {
+                a *= 10;
+                a += (int)(input[index] - '0');
+                index++;
+            }
+            index++;
+            while ((input[index] >= '0') && (input[index] <= '9')) {
+                b *= 10;
+                b += (int)(input[index] - '0');
+                index++;
+            }
+            char buffer[256];
+            itoa(a+b, buffer, 10);
+            kprint(buffer);
+            kprint("\n");
+        } else {
+            kprint("Unrecognized Command: ");
+            kprint(input);
+            kprint("\n");
+        }
     }
-    kprint(input);
-    kprint("\n");
 }
